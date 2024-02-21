55c0787f979e0bb69e812604eecd63176f43ec33
Steven John <149338927+zzsjnvts@users.noreply.github.com>
2024-02-22 05:50:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,10 @@ UPPER_COLOR = [150,255,255]
 KERNEL_SIZE = (3, 3)
 DILATING = 3
 DEBUGGING = False 
-
+print("version 1.1")
 def themagicpress():
+    keyboard.press('k')
+    keyboard.press('k')
     keyboard.press('k')
     keyboard.release('k')
 
